c124587a7a50e1ed99a959a9b4e8782fa4b81f57
Pyx <git@notpyx.me>
2024-12-22 15:59:07 -0500

    Remove src/administer (1.2.3 codebase)

--- src/ServerScriptService/Administer/AdministerMainPanel/Main/Configuration/Admins/NewAdmin/SideDecor/ImageLabel/Script.client.lua
+++
@@ -1,6 +0,0 @@
-while true do
-	local tween = game:GetService("TweenService"):Create(script.Parent, TweenInfo.new(0.3,Enum.EasingStyle.Linear, Enum.EasingDirection.In,0,false,0),{Rotation=360})
-	tween:Play()
-	tween.Completed:Wait()
-	script.Parent.Rotation = 0
-end
--- src/ServerScriptService/Administer/AdministerMainPanel/Main/Configuration/InfoPage/Credits/log/UsesChange.client.lua
+++
@@ -1,8 +0,0 @@
-script.Parent.Text =
-	"Administer is powered by many open-source resources."..
-	"\nButton Animations by Qinrir"..
-	"\nIcons by FlatIcon"..
-	"\nQuickBlur by @pixe_ated"..
-	"\nShime (modified) by @winnerstakesall"..
-	"\nFull stack programming & UI/UX by PyxFluff (2024)"..
-	"\nCore apps developed by Administer Team members (2024)"
